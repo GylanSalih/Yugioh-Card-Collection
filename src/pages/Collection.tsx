80509f0926e9b47616702cb39495d7d03be0209e
Developer <dev@example.com>
2026-03-29 17:29:25 +0200

    Fix unused imports warning

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './Collection.module.scss';
 import YGOProDeckService, { Card } from '../services/ygoProDeckService';
 
